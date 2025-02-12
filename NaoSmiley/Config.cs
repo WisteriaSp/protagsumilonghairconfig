3700411388a266691890acac6d4a9c1b5129511d
WisteriaSp <wisteriaaofficial@gmail.com>
2025-02-12 01:50:40 -0800

    naosmiley

---
+++ NaoSmiley/Config.cs
@@ -0,0 +1,47 @@
+﻿using NaoSmiley.Template.Configuration;
+using Reloaded.Mod.Interfaces.Structs;
+using System.ComponentModel;
+using CriFs.V2.Hook;
+using CriFs.V2.Hook.Interfaces;
+using System.Reflection;
+
+namespace NaoSmiley.Configuration
+{
+	public class Config : Configurable<Config>
+	{
+		/*
+            User Properties:
+                - Please put all of your configurable properties here.
+
+            By default, configuration saves as "Config.json" in mod user config folder.    
+            Need more config files/classes? See Configuration.cs
+
+            Available Attributes:
+            - Category
+            - DisplayName
+            - Description
+            - DefaultValue
+
+            // Technically Supported but not Useful
+            - Browsable
+            - Localizable
+
+            The `DefaultValue` attribute is used as part of the `Reset` button in Reloaded-Launcher.
+        */
+
+        [Category("Test")]
+        [DisplayName("Test")]
+		[Description("Bool example")]
+		[DefaultValue(true)]
+		public bool Test1 { get; set; } = true; // bool used in Mod.CS, not the folder name, but the bool name
+    }
+
+    /// <summary>
+    /// Allows you to override certain aspects of the configuration creation process (e.g. create multiple configurations).
+    /// Override elements in <see cref="ConfiguratorMixinBase"/> for finer control.
+    /// </summary>
+	public class ConfiguratorMixin : ConfiguratorMixinBase
+	{
+		// 
+	}
+}
---
+++ NaoSmiley/Mod.cs
@@ -0,0 +1,146 @@
+﻿using NaoSmiley.Configuration;
+using NaoSmiley.Template;
+using Reloaded.Hooks.ReloadedII.Interfaces;
+using Reloaded.Mod.Interfaces;
+using CriFs.V2.Hook;
+using CriFs.V2.Hook.Interfaces;
+using PAK.Stream.Emulator;
+using PAK.Stream.Emulator.Interfaces;
+using Reloaded.Mod.Interfaces.Internal;
+using BGME.Framework;
+using BGME.Framework.Interfaces;
+using BF.File.Emulator;
+using BF.File.Emulator.Interfaces;
+
+namespace NaoSmiley
+{
+	/// <summary>
+	/// Your mod logic goes here.
+	/// </summary>
+	public class Mod : ModBase // <= Do not Remove.
+	{
+		/// <summary>
+		/// Provides access to the mod loader API.
+		/// </summary>
+		private readonly IModLoader _modLoader;
+	
+		/// <summary>
+		/// Provides access to the Reloaded.Hooks API.
+		/// </summary>
+		/// <remarks>This is null if you remove dependency on Reloaded.SharedLib.Hooks in your mod.</remarks>
+		private readonly IReloadedHooks? _hooks;
+	
+		/// <summary>
+		/// Provides access to the Reloaded logger.
+		/// </summary>
+		private readonly ILogger _logger;
+	
+		/// <summary>
+		/// Entry point into the mod, instance that created this class.
+		/// </summary>
+		private readonly IMod _owner;
+	
+		/// <summary>
+		/// Provides access to this mod's configuration.
+		/// </summary>
+		private Config _configuration;
+	
+		/// <summary>
+		/// The configuration of the currently executing mod.
+		/// </summary>
+		private readonly IModConfig _modConfig;
+	
+		public Mod(ModContext context)
+		{
+			_modLoader = context.ModLoader;
+			_hooks = context.Hooks;
+			_logger = context.Logger;
+			_owner = context.Owner;
+			_configuration = context.Configuration;
+			_modConfig = context.ModConfig;
+
+			var modDir = _modLoader.GetDirectoryForModId(_modConfig.ModId); // modDir variable for file emulation
+
+			// For more information about this template, please see
+			// https://reloaded-project.github.io/Reloaded-II/ModTemplate/
+
+			// If you want to implement e.g. unload support in your mod,
+			// and some other neat features, override the methods in ModBase.
+
+			// TODO: Implement some mod logic
+
+			// Define controllers and other variables, set warning messages
+	
+			var criFsController = _modLoader.GetController<ICriFsRedirectorApi>();
+			if (criFsController == null || !criFsController.TryGetTarget(out var criFsApi))
+			{
+				_logger.WriteLine($"Something in CriFS shit its pants! Normal files will not load properly!", System.Drawing.Color.Red);
+				return;
+            }
+
+            var PakEmulatorController = _modLoader.GetController<IPakEmulator>();
+            if (PakEmulatorController == null || !PakEmulatorController.TryGetTarget(out var _PakEmulator))
+            {
+                _logger.WriteLine($"Something in PAK Emulator shit its pants! Files requiring bin merging will not load properly!", System.Drawing.Color.Red);
+                return;
+            }
+
+            var BfEmulatorController = _modLoader.GetController<IBfEmulator>();
+            if (BfEmulatorController == null || !BfEmulatorController.TryGetTarget(out var _BfEmulator))
+            {
+                _logger.WriteLine($"Something in BF Emulator shit its pants! Files requiring bf merging will not load properly!", System.Drawing.Color.Red);
+                return;
+            }
+
+            var BGMEController = _modLoader.GetController<IBgmeApi>();
+			if (BGMEController == null || !BGMEController.TryGetTarget(out var _BGME))
+			{
+				_logger.WriteLine($"Something in BGME shit its pants! Files requiring bin merging will not load properly!", System.Drawing.Color.Red);
+				return;
+            }
+
+
+            // Set configuration options - obviously you don't need all of these, pick and choose what you need!
+
+            // criFS
+            if (_configuration.Test1)
+			{
+				criFsApi.AddProbingPath("Test"); // folder path. place a subfolder inside and then start your file path. for example: "(mod folder)\Test\(any name)\..."
+			}
+
+            // PAK Emulator
+            if (_configuration.Test1)
+			{
+				_PakEmulator.AddDirectory(Path.Combine(modDir, "Test")); // folder path. immediately start your file path inside this folder. for example: "(mod folder)\Test\..."
+            }
+
+            // BF Emulator
+            if (_configuration.Test1)
+            {
+                _BfEmulator.AddDirectory(Path.Combine(modDir, "Test")); // folder path. immediately start your file path inside this folder. for example: "(mod folder)\Test\..."
+            }
+
+            // BGME
+            if (_configuration.Test1)
+            {
+				_BGME.AddFolder(Path.Combine(modDir, "Test")); // folder path. immediately start your file path inside this folder. for example: "(mod folder)\Test\..."
+            }
+        }
+	
+		#region Standard Overrides
+	public override void ConfigurationUpdated(Config configuration)
+	{
+		// Apply settings from configuration.
+		// ... your code here.
+		_configuration = configuration;
+		_logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
+	}
+	#endregion
+	
+		#region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+	public Mod() { }
+#pragma warning restore CS8618
+	#endregion
+	}
+}
